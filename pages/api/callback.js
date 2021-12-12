bcb300872ab6978853fce9f953113c74cdb4bbc3
achmad.jamaludin <achmad.jamaludin@online-pajak.com>
2021-12-13 01:36:21 +0700

    init

--- pages/api/callback.js
+++ pages/api/callback.js
@@ -1,10 +1,18 @@
-import auth0 from '../../lib/auth0'
+import auth0 from "../../lib/auth0";
+
+const afterCallback = (req, res, session, state) => {
+  console.log({ session, state });
+  if (!session.user.isAdmin) {
+    throw new UnauthorizedError("User is not admin");
+  }
+  return session;
+};
 
 export default async function callback(req, res) {
   try {
-    await auth0.handleCallback(req, res)
+    await auth0.handleCallback(req, res, { afterCallback });
   } catch (error) {
-    console.error(error)
-    res.status(error.status || 500).end(error.message)
+    console.error(error);
+    res.status(error.status || 500).end(error.message);
   }
 }
