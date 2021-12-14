019fdbc67c389671712c5362ce8b09b20999e1b8
achmad.jamaludin <achmad.jamaludin@online-pajak.com>
2021-12-14 17:16:47 +0700

    change all file inside pages from js to jsx

--- pages/profile.jsx
+++ pages/profile.jsx
@@ -1,7 +1,7 @@
 // This import is only needed when checking authentication status directly from getInitialProps
 // import auth0 from '../lib/auth0'
-import { useFetchUser } from '../lib/user'
-import Layout from '../components/layout'
+import { useFetchUser } from "../lib/user";
+import Layout from "../components/layout";
 
 function ProfileCard({ user }) {
   return (
@@ -15,17 +15,17 @@ function ProfileCard({ user }) {
         <p>name: {user.name}</p>
       </div>
     </>
-  )
+  );
 }
 
 function Profile() {
-  const { user, loading } = useFetchUser({ required: true })
+  const { user, loading } = useFetchUser({ required: true });
 
   return (
     <Layout user={user} loading={loading}>
       {loading ? <>Loading...</> : <ProfileCard user={user} />}
     </Layout>
-  )
+  );
 }
 
-export default Profile
+export default Profile;
